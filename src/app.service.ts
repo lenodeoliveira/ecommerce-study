6945e350238f5261babd4d703a70d0d5b8b1cde9
Lennon <lenodeoliveira@gmail.com>
2024-04-19 22:15:31 -0300

    feat: initialize nestjs project

---
+++ src/app.service.ts
@@ -0,0 +1,33 @@
+import { Get, Injectable } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+
+@Injectable()
+export class AppService {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  async healthCheck(): Promise<{
+    status: string;
+    error: unknown;
+    dataBaseStatus: string;
+  }> {
+    const res: HealthCheckResult = await this.health.check([
+      () => this.db.pingCheck('database'),
+    ]);
+    console.log(res);
+    return {
+      status: res?.status,
+      dataBaseStatus: res?.info?.database?.status,
+      error: res?.error ?? null,
+    };
+  }
+}
